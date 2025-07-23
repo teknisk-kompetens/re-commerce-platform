2ef3bfe850361aca7c1591d1e72a46efe52c5f02
teknisk-kompetens <129761495+teknisk-kompetens@users.noreply.github.com>
2025-07-24 00:31:28 +0200

    Update route.ts

--- app/api/features/route.ts
+++ app/api/features/route.ts
@@ -37,7 +37,7 @@ export async function GET(request: NextRequest) {
       )
     }
 
-    const featuresWithVotes = platform.features.map(feature => {
+    const featuresWithVotes = platform.features.map(feature: any) => {
       const votes = {
         dislike: 0,
         like: 0,
